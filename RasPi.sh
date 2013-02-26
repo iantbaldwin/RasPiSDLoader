bed32744fe57718ef47ea99fa0562e70097e9fa1
Ian Baldwin <iantbaldwin@mac.com>
2013-02-26 01:40:42 -0500

    Comment space

--- RasPi.sh
+++ RasPi.sh
@@ -51,6 +51,7 @@ read -e RPImage
 sudo dd bs=1m if=$RPImage of=/dev/rdisk1
 
 # Verify successful restore
+
 # Get disk name
 diskutil info /dev/rdisk1s1 | grep -i 'Mount Point:' > /tmp/volNameInt.txt
 volNameFin=$(awk '{print $3}' /tmp/volNameInt.txt)
